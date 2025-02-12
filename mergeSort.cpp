5050e0856417154da60011b17ebba180837a7b0a
Pratik <pratikjha4926@gmail.com>
2025-02-12 13:50:02 +0545

    merge and quick sort

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -3,7 +3,42 @@
 
 using namespace std;
 
-int main(){
+void merge(int *arr, int low, int mid, int high) {
+    int size = high - low + 1;  
+    int temp[size];  
+    int left = low, right = mid + 1, temp_index = 0;
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp[temp_index++] = arr[left++];
+        } else {
+            temp[temp_index++] = arr[right++];
+        }
+    }
+    while (left <= mid) {
+        temp[temp_index++] = arr[left++];
+    }
+    while (right <= high) {
+        temp[temp_index++] = arr[right++];
+    }
+    for (int i = 0; i < size; i++) {
+        arr[low + i] = temp[i];
+    }
+}
+
+void ms(int *arr, int low, int high) {
+    if (low >= high) return;
+    int mid = (low + high) / 2;
+    ms(arr, low, mid);
+    ms(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+
+int main() {
+    int arr[9] = {9, 7, 8, 6, 5, 4, 3, 2, 1};
+    ms(arr, 0, 8);
     
+    for (int i = 0; i < 9; i++) {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
--- quickSort.cpp
+++ quickSort.cpp
@@ -3,7 +3,34 @@
 
 using namespace std;
 
+int partition(int *arr, int low, int high){
+    int i = low, j = high, pivot = arr[low];
+    while(i<j){
+        while(arr[i]<=pivot && i<high){
+            i++;
+        }
+        while(arr[j]>pivot && j>low){
+            j--;
+        }
+        if (i<j) swap(arr[i],arr[j]);
+    }
+    swap(arr[low],arr[j]);
+    return j;
+}
+
+void qs(int *arr, int low, int high){
+    if (low < high){
+        int pIndex= partition(arr,low,high);
+        qs(arr, low, pIndex-1);
+        qs(arr, pIndex+1, high);
+    }
+}
+
 int main(){
-    
+    int arr[9] = {9, 7, 8, 6,5, 4, 3, 2, 1};
+    qs(arr,0,8);
+    for (int i = 0; i<=8; i++){
+        cout << arr[i] << " ";
+    }
     return 0;
 }
